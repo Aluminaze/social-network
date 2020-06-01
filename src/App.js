4826e0cd8e22c78842bcd506177b724d888d0697
unknown <aluminaze@gmail.com>
2020-06-02 00:31:39 +0200

    Added menu components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './style/css/App.css';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="container">
         <Header />
-
         <section className="wrapper">
           <NavBar />
           <Switch>
@@ -20,6 +22,15 @@ const App = () => {
             </Route>
             <Route path="/dialogs">
               <Dialogs />
+            </Route>
+            <Route path="/news">
+              <News />
+            </Route>
+            <Route path="/music">
+              <Music />
+            </Route>
+            <Route path="/settings">
+              <Settings />
             </Route>
           </Switch>
         </section>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import style from './NavBar.module.css'
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <nav className={style.nav}>
           <ul>
-            <li className={style.item}><a className={style.link} href="/profile">Profile</a></li>
-            <li className={style.item}><a className={style.link} href="/dialogs">Messages</a></li>
-            <li className={style.item}><a className={style.link} href="#">News</a></li>
-            <li className={style.item}><a className={style.link} href="#">Music</a></li>
+            <li className={style.item}><NavLink className={style.link} to="/profile">Profile</NavLink></li>
+            <li className={style.item}><NavLink className={style.link} to="/dialogs">Messages</NavLink></li>
+            <li className={style.item}><NavLink className={style.link} to="/news">News</NavLink></li>
+            <li className={style.item}><NavLink className={style.link} to="/music">Music</NavLink></li>
           </ul>
           <ul>
-          <li className={style.item}><a className={style.link} href="#">Settings</a></li>
+          <li className={style.item}><NavLink className={style.link} to="/settings">Settings</NavLink></li>
           </ul>
         </nav>
     )
