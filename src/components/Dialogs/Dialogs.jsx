f6005bf0e66c100e4ba33fa39937e90601bdddb1
unknown <aluminaze@gmail.com>
2020-06-02 16:43:18 +0200

    Message page created

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,10 +3,37 @@ import style from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
-        </div>
+        <section className={style.dialogs}>
+            <div className={style.dialogs__list}>
+                <div className={style.dialogs__item}>
+                    Alex
+                </div>
+                <div className={`${style.dialogs__item} ${style.dialogs__active}`}>
+                    Thomas
+                </div>
+                <div className={style.dialogs__item}>
+                    Jessi
+                </div>
+                <div className={style.dialogs__item}>
+                    Brad
+                </div>
+                <div className={style.dialogs__item}>
+                    Angel
+                </div>
+                <div className={style.dialogs__item}>
+                    Bessy
+                </div>
+            </div>
+            <div className={style.dialogs__messages}>
+                <div className={style.dialogs__message}>
+                    Hey!
+                </div>
+                <div className={style.dialogs__message}>
+                    Whatsaaaaaaap?
+                </div>
+            </div>
+        </section>
     )
-} 
+}
 
 export default Dialogs;
