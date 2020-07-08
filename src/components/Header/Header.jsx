2c6d90cb9d3fa8df3b02412a63c75c00cc9dd230
unknown <aluminaze@gmail.com>
2020-07-08 14:14:59 +0200

    change header logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import style from './Header.module.css'
+import React from "react";
+import style from "./Header.module.css";
+import { ReactComponent as Logo } from "./header_icon.svg";
 
 const Header = () => {
   return (
     <header className={style.header}>
-      <img className={style.logo} src="https://cdn.dribbble.com/users/2087607/screenshots/6451099/free-youtube-comments-icon-png-download.jpg" />
+      <Logo className={style.logo} />
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
