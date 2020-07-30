aeea104b28f976c10703530719d5d07296733786
unknown <aluminaze@gmail.com>
2020-07-30 18:51:07 +0200

    update post form in Posts component

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import style from './Posts.module.css'
 import Post from './Post/Post'
+import { Field, reduxForm } from 'redux-form';
+
+let PostForm = (props) => {
+  return (
+    <form className={style.posts__form} onSubmit={props.handleSubmit}>
+      <Field className={style.posts__form__input} component="textarea" name="input" type="text" />
+      <button className={style.posts__form__submit} type="submit">Add post</button>
+    </form>    
+  )
+}
+
+PostForm = reduxForm({
+  form: "post"
+})(PostForm)
 
 const Posts = (props) => {
   let newPostElement = React.createRef()
@@ -17,10 +31,15 @@ const Posts = (props) => {
 
   let postsItems = props.postsData.map(post => <Post profile="I'm" message={post.message} key={post.id} likes={post.likes} />)
 
+  const onSubmit = (newPostText) => {
+    console.log(newPostText)
+  }
+
   return (
     <div className={style.profile__posts}>
       <div className={style.posts__header}>My posts</div>
-      <div className={style.posts__form}>
+      <PostForm onSubmit={onSubmit} />
+      {/* <div className={style.posts__form}>
         <textarea
           onChange={onPostChange}
           ref={newPostElement}
@@ -31,7 +50,7 @@ const Posts = (props) => {
           onClick={onAddPost}
           className={style.posts__form__submit}
         >Add post</button>
-      </div>
+      </div> */}
       {postsItems}
     </div>
   )
